cdf258c981ecd43a2b782267a4049e6369c321c5
TaNeisha Wilks <twilks18@gmail.com>
2016-09-01 20:02:22 -0500

    update info

--- user-signup/main.py
+++ user-signup/main.py
@@ -1,23 +1,24 @@
 
+#
 import webapp2
 import re
 import cgi
 
 user_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 psswrd_re= re.compile(r"^.{3,20}$")
-email_re= re.compile(r'^[/S]+@[\S]+.[\s]+$')
+email_re= re.compile(r"^[\S]+@[\S]+.[\S]+$")
 
 def escape_symbols(s):
     return cgi.escape(s, quotes = True)
 
 def valid_username(username):
-     return user_re.match(username)
+     return username and user_re.match(username)
 
 def valid_passwrd(password):
-    return psswrd_re.match(password)
+    return password and  psswrd_re.match(password)
 
 def valid_email(email):
-     return email or email_re.match(email)
+    return not email or email_re.match(email)
 
 
 page_header = """
@@ -39,30 +40,27 @@ sign_up="""
     <form action='/' method="post">
         <label>
             Username
-            <input type="text" name="username" value="%(username)s"/>
-            <div>%(error_username)s</div>
+            <input type="text" name="username" value="%(username)s"/> %(user_error)s
+          </label>
+
         </label>
         <br>
         <br>
         <label>
             Password
-            <input type="password" name="password"/>
-            <div>%(error_password)s</div>
+            <input type="password" name="password"/> %(psswrd)s
         </label>
         <br>
         <br>
         <label>
             Verify
-            <input type="password" name="verify"/>
-            <div>%(error_verify)s</div>error_verify
+            <input type="password" name="verify"/> %(verify)s
         </label>
         <br>
         <br>
         <label>
             Email (optional)
-            <input type="text" name="email" value="%(email)s"/>
-            <div>%(error_email)s</div>
-        </label>
+            <input type="text" name="email" value="%(email)s"/> %(email_error)s
         <br>
         <br>
         <input type="submit" value="Submit"/>
@@ -72,24 +70,23 @@ sign_up="""
 page_content = page_header + sign_up + page_footer
 
 
+
 class MainHandler(webapp2.RequestHandler):
-    def write_form(self, errors=None, username="", email=""):
-        if errors is None:
-            errors = {}
-        env = {
-            "username":username,
-            "email":email,
-            error_username: '', error_password: '', error_verify: '', error_email: ''
-            }
-        env.update(errors)
-        self.response.write(page_content % env)
+
+
+    def write_form(self,user_error="", username="", psswrd="", verify="", email="", email_error=""):
+        self.response.write(page_content %{'username':username,'user_error':user_error,'email':email,'email_error':email_error,'psswrd':psswrd,'verify':verify })
+
+
 
     def get(self):
-
         self.write_form()
 
     def post(self):
-        errors={}
+
+
+
+
         userName = self.request.get('username')
         passWord = self.request.get('password')
         eMail = self.request.get('email')
@@ -99,39 +96,48 @@ class MainHandler(webapp2.RequestHandler):
         password_1 = valid_passwrd(passWord)
         email_1 = valid_email(eMail)
 
-        if not username_1:
-            errors["error_username"]="invalid username"
+        if not username_1 or (userName == ""):
 
-        if not password_1:
-            errors["error_password"]="invalid password"
-
-        if not (passWord == veriFy):
-            errors["error_verify"]="Passwords do not match"
+            self.write_form("Invalid Username",userName)
 
 
-        if not (email_1):
-            errors["error_email"]="invalid email"
 
 
-        if not(username_1 and password_1 and (passWord == veriFy)):
-            self.write_form(errors, userName, eMail)
+        elif not password_1:
+            self.write_form("",userName,"Invalid  Password")
+
+
+
+        elif not (passWord == veriFy):
+            self.write_form("",userName,"","Passwords Do Not Match")
+
+        elif username_1 and password_1 and veriFy:
+            self.redirect('/thanks?username=%s' % userName)
+
+
+
+
+        elif not email_1 or (eMail == ""):
+
+            self.write_form("",userName,"","",eMail,"Invalid Email")
+
+
+
         else:
-            self.response.write("That's Great")
+            self.redirect('/thanks?username=%s' % userName)
 
 
 class thanksHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write("thanks")
 
-
-
-
-
+        welcome_user= self.request.get('username')
+        welcome = "Welcome " + welcome_user + "!"
+        self.response.write(welcome)
 
 
 
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),('/thanks',thanksHandler)
+    ('/', MainHandler),('/thanks', thanksHandler)
 ], debug=True)
